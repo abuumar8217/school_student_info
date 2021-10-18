8ba3aa2516d6fb2fb89dfe4e5d3dac581deac931
abuumar8217 <92126382+abuumar8217@users.noreply.github.com>
2021-10-18 16:14:15 -0700

    Add files via upload

---
+++ talaba_amal.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 11:57:10 2021
+
+@author: abuumar
+"""
+
+class Shaxs:
+    """Shaxslar haqida ma'lumot"""
+
+    def __init__(self, ism, familiya, passport, tyil):
+        """Shaxsning xususiyatlari"""
+        self.ism = ism
+        self.familiya = familiya
+        self.passport = passport
+        self.tyil = tyil
+
+    def get_info(self):
+        """Shaxs haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
+        return info
+
+    def get_age(self, yil):
+        """Shaxsning yoshini qaytaruvchi metod"""
+        return yil - self.tyil
+
+
+class Talaba(Shaxs):
+    """Talaba klassi"""
+
+    def __init__(self, ism, familiya, passport, tyil, idraqam, fanlar):
+        """Talabaning xususiyatlari"""
+        super().__init__(ism, familiya, passport, tyil)
+        self.idraqam = idraqam
+        self.bosqich = 1
+        self.fanlar=fanlar
+        
+
+    def get_id(self):
+        """Talabaning ID raqami"""
+        return self.idraqam
+
+    def get_bosqich(self):
+        """Talabaning o'qish bosqichi"""
+        return self.bosqich
+
+    def get_info(self):
+        """Talaba haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
+        return info
+   
+   
+            
+class Fan:
+    def __init__(self,fanlar):
+        self.fanlar=fanlar
+    
+    def fanga_yozil(self,fan):
+        self.fanlar.append(fan)
+        
+    
+    def get_info(self):
+        """Talaba haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
+        info += f"Qatnashadigan fanlar: {self.fanlar}"
+        return info
+    
+fan_list = Fan=(["Uzbek",])
+talaba = Talaba("Valijon","Aliyev","FA112299",2000,"0000012",fan_list)    
+        
